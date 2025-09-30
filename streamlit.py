4a83fcea6f6673ac1cc4643ef90b2069dc900ec6
Iago Monfredini <iagomonfredini46171@gmail.com>
2025-09-30 15:01:00 -0300

    corrigindo erros

--- streamlit.py
+++ streamlit.py
@@ -36,7 +36,7 @@ cursor.execute('''
 conexao.commit()
 
 st.title("Sistema de Biblioteca")
-dados = op.listar_livros
+dados = op.listar_livros()
 menu = ["Cadastrar Livro", "Listar Livros", "Atualizar Livro", "Remover Livro"]
 escolha = st.sidebar.selectbox("Menu", menu)
 
@@ -49,40 +49,41 @@ if escolha == "Cadastrar Livro":
         disponivel = st.selectbox("Disponibilidade", ["Sim", "Não"])
 
 if st.button("Cadastrar"):
-        if titulo and autor and ano:
-            op.cadastrar_livros(titulo.capitalize(), autor.capitalize(), ano.capitalize(), disponivel.capitalize())
-        else:
-            st.warning("Preencha todos os campos!")
+    if titulo and autor and ano:
+        ano = str(ano.year)
+        op.cadastrar_livros(titulo.capitalize(), autor.capitalize(), ano.capitalize(), disponivel.capitalize())
+    else:
+        st.warning("Preencha todos os campos!")
 
 elif escolha == "Listar Livros":
-        st.header("Lista de Livros")
-        dados = op.listar_livros()
-        if dados:
-            st.table(dados)
-        else:
-            st.info("Nenhum livro cadastrado.")
+    st.header("Lista de Livros")
+    dados = op.listar_livros()
+    if dados:
+        st.table(dados)
+    else:
+        st.info("Nenhum livro cadastrado.")
 
 elif escolha == "Atualizar Livro":
-        st.header("Atualizar Livro")
-        dados = op.listar_livros()
-        if dados:
-            st.dataframe(dados)
-            id = st.number_input("Digite o ID do livro a ser atualizado:", min_value=1, step=1)
-            campo = st.selectbox(f"Campo a atualizar", ["titulo", "autor", "ano", "disponivel"])
-            novo_valor = st.text_input("Novo valor")
+    st.header("Atualizar Livro")
+    dados = op.listar_livros()
+    if dados:
+        st.dataframe(dados)
+        id = st.number_input("Digite o ID do livro a ser atualizado:", min_value=1, step=1)
+        campo = st.selectbox(f"Campo a atualizar", ["titulo", "autor", "ano", "disponivel"])
+        novo_valor = st.text_input("Novo valor")
 
-        if st.button("Atualizar"):
-            op.atualizar_livros(campo, novo_valor, id)
-        else:
-            st.info("Nenhum livro para atualizar.")
+    if st.button("Atualizar"):
+        op.atualizar_livros(campo, novo_valor, id)
+    else:
+        st.info("Nenhum livro para atualizar.")
 
 elif escolha == "Remover Livro":
-        st.header("Remover Livro")
-        dados = op.listar_livros()
-        if dados:
-            st.dataframe(dados)
-            id = st.number_input("Digite o ID do livro a ser removido:", min_value=1, step=1)
-            if st.button("Remover"):
-                op.deletar_livro(id)
-        else:
-            st.warning("Nenhum livro para remover.")
+    st.header("Remover Livro")
+    dados = op.listar_livros()
+    if dados:
+        st.dataframe(dados)
+        id = st.number_input("Digite o ID do livro a ser removido:", min_value=1, step=1)
+        if st.button("Remover"):
+            op.deletar_livro(id)
+    else:
+        st.warning("Nenhum livro para remover.")
